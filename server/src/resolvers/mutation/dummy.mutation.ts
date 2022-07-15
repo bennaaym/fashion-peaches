fb34e5fb467c68e514c93f943ed98c9121b950e7
bennaaym <mr.aymen.bennabi@gmail.com>
2022-07-15 22:55:46 +0300

    :tada: set up server

---
+++ server/src/resolvers/mutation/dummy.mutation.ts
@@ -0,0 +1,3 @@
+export const DummyMutation = {
+  dummy: () => 'dummy',
+};
---
+++ server/src/schemas/schema.ts
@@ -0,0 +1,11 @@
+import { gql } from 'apollo-server';
+
+export const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+
+  type Mutation {
+    dummy: String!
+  }
+`;
